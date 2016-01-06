b455d7f1923eccfc119d23fbd2d54a49eda98c9f
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-01-06 08:10:31 +0000

    Initial commit

---
+++ OSMapKitAdapter/OSMapKitAdapter.h
@@ -0,0 +1,19 @@
+//
+//  OSMapKitAdapter.h
+//  OSMapKitAdapter
+//
+//  Created by Dave Hardiman on 06/01/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OSMapKitAdapter.
+FOUNDATION_EXPORT double OSMapKitAdapterVersionNumber;
+
+//! Project version string for OSMapKitAdapter.
+FOUNDATION_EXPORT const unsigned char OSMapKitAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSMapKitAdapter/PublicHeader.h>
+
+
